5148791d1fe98fec0af65264899836ecffe3201c
Jack2310w <m15900484770@163.com>
2024-07-18 23:50:01 +0800

    Lab Xv6 and Unix utilities - pingpong v2

--- user/pingpong.c
+++ user/pingpong.c
@@ -14,18 +14,14 @@ int main()
   {
     // 子进程的操作
     pid = getpid();
-    int fdread = dup(p1[1]); // 读取父进程传输数据的fd
-    int fdwrite = dup(p2[0]); // 向父进程传输数据的fd
     char buf; // 用于接收管道数据
     // 关闭不需要的fd
     close(p1[0]);
-    close(p1[1]);
-    close(p2[0]);
     close(p2[1]);
     
-    read(fdread, &buf, 1); // 读取父进程的数据
+    read(p1[1], &buf, 1); // 读取父进程的数据
     printf("%d: received ping\n", pid);
-    write(fdwrite, &buf, 1); // 向父进程发送数据
+    write(p2[0], &buf, 1); // 向父进程发送数据
     
     exit(0);
   }
@@ -33,23 +29,18 @@ int main()
   {
     // 父进程的操作
     pid = getpid();
-    int fdwrite = dup(p1[0]); // 向子进程传输数据的fd
-    int fdread = dup(p2[1]); // 读取子进程传输数据的fd
     char buf = 0; // 要传输的字节
     char rdbuf; // 读取的字节
     // 关闭不需要的fd
-    close(p1[0]);
     close(p1[1]);
     close(p2[0]);
-    close(p2[1]);
     
-    write(fdwrite, &buf, 1); // 向子进程发送数据
+    write(p1[0], &buf, 1); // 向子进程发送数据
     wait(0);
-    read(fdread, &rdbuf, 1); // 读取子进程传输的数据
+    read(p2[1], &rdbuf, 1); // 读取子进程传输的数据
     printf("%d: received pong\n", pid);
     
     exit(0);
   }
 }
-
 
